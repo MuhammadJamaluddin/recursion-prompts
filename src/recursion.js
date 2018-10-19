81a7e8ca0b29db3fb3411e22e4cde0d55e6f3ce3
Muhammad <mohamed.eldeen.399@gmail.com>
2018-10-19 13:52:32 +0200

    Implement the palindrome function recursively

--- src/recursion.js
+++ src/recursion.js
@@ -115,6 +115,15 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+	if (string[0].toLocaleLowerCase() === string[string.length-1].toLocaleLowerCase()) {
+		if (string.length === 1 || string.length === 2 || string.length === 3) {
+			return true;
+		}
+
+		return palindrome(string.slice(1, string.length-1));
+	} else {
+		return false;
+	}
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
